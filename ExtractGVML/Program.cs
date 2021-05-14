29d5bbc2cf0900dbd5ce5648c999774d915aeb87
Rohit Agrawalla <rohit.agrawalla@gmail.com>
2021-05-14 11:59:27 -0700

    Add project files.

---
+++ ExtractGVML/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			var dataObject = Clipboard.GetDataObject();
+			var formats = dataObject.GetFormats();
+
+			foreach (var format in formats)
+			{
+				Console.Out.WriteLine(format);
+			}
+
+			var data = dataObject.GetData("Art::GVML ClipFormat");
+
+			if (data != null)
+			{
+				var file = File.Create("c:\\users\\rohit\\desktop\\GVML.zip");
+				var ms = data as MemoryStream;
+				var bytes = ms.ToArray();
+				file.Write(bytes, 0, bytes.Length);
+				file.Close();
+			}
+		}
+	}
+}
